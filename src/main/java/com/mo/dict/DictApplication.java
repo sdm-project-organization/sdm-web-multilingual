cb96ac18601e128c8b4a4f65f86573f88feabdd1
supermoon <supermoon@nexon.co.kr>
2019-07-17 18:07:11 +0900

    check-logic

--- src/main/java/com/mo/dict/DictApplication.java
+++ src/main/java/com/mo/dict/DictApplication.java
@@ -1,13 +1,32 @@
 package com.mo.dict;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 //@EnableDiscoveryClient
 public class DictApplication extends SpringBootServletInitializer {
+
+    @Value("${spring.profiles}")
+    String profiles;
+
+    @Value("${server.port}")
+    String port;
+
+    @Value("${eureka.instance.hostname}")
+    String discovery;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("profiles : " + profiles);
+        System.out.println("port : " + port);
+        System.out.println("discovery : " + discovery);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DictApplication.class, args);
